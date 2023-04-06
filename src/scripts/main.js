10d430d5584174f002dd7e37f2e35a0234b9e3c3
Andrey Lehendzevych <andrey.lehendzevych@gmail.com>
2023-04-06 12:09:11 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,31 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = e.target.closest('.wall');
+
+  if (!wall) {
+    return;
+  }
+
+  const spider = document.querySelector('.spider');
+  let spiderY = e.offsetY - spider.offsetHeight / 2;
+  let spiderX = e.offsetX - spider.offsetWidth / 2;
+
+  const maxY = wall.clientHeight - spider.offsetHeight;
+  const maxX = wall.clientWidth - spider.offsetWidth;
+
+  if (spiderY < 0) {
+    spiderY = 0;
+  } else if (spiderY > maxY) {
+    spiderY = maxY;
+  }
+
+  if (spiderX < 0) {
+    spiderX = 0;
+  } else if (spiderX > maxX) {
+    spiderX = maxX;
+  }
+
+  spider.style.top = `${spiderY}px`;
+  spider.style.left = `${spiderX}px`;
 });
